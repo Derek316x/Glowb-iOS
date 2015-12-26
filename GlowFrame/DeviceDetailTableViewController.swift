4d386a9192a6637ba0323b31f0ae9ae2fa3bd336
mikekavouras <kavourasm@gmail.com>
2015-12-26 01:15:39 -0500

    device detail view

---
+++ GlowFrame/DeviceDetailTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DeviceDetailTableViewController.swift
+//  GlowFrame
+//
+//  Created by Michael Kavouras on 12/26/15.
+//  Copyright © 2015 Michael Kavouras. All rights reserved.
+//
+
+import UIKit
+
+class DeviceDetailTableViewController: UITableViewController {
+
+}
